680414b08a6dfaed445b304381d2fb866a1cb43c
Mathias Chevez <davidchevez90@gmail.com>
2024-11-16 20:06:22 -0700

    add comment functionality to doc editor

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -10,9 +10,7 @@ import Image from 'next/image'
 import { updateDocument } from '@/lib/actions/room.actions'
 import Loader from './Loader'
 
-const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => {
-  const currentUserType = 'editor'
-
+const CollaborativeRoom = ({ roomId, roomMetadata, currentUserType }: CollaborativeRoomProps) => {
   const [editing, setEditing] = useState(false);
   const [loading, setLoading] = useState(false);
   const [title, setTitle] = useState(roomMetadata.title);
@@ -97,7 +95,7 @@ const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => 
               </SignedIn>
             </div>
           </Header>
-          <Editor />
+          <Editor roomId={roomId} currentUserType={currentUserType} />
         </div>
       </ClientSideSuspense>
     </RoomProvider>
